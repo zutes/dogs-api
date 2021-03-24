a036f76f0a7b2947d1df6fa22132078a02a6398f
zutes <zutes3479@gmail.com>
2021-03-24 15:13:44 -0600

    Create array
    Create initial dog api array.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var dogList = [
+  {
+    name: 'Cockapoo',
+    image: 'https://images.dog.ceo/breeds/cockapoo/bubbles2.jpg'
+  },
+  {
+    name: 'Eskimo',
+    image: 'https://images.dog.ceo/breeds/eskimo/n02109961_10021.jpg' 
+  },
+  {
+    name: 'pointer',
+    image: 'https://images.dog.ceo/breeds/husky/n02110185_10844.jpg'
+  }
+];
