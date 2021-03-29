32e8ece78807a0527cab6ef5ff683aacb2bcf2f3
zutes <zutes3479@gmail.com>
2021-03-29 14:18:27 -0600

    Update array/forEach loop
    Updated the array keys.
    Created forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 var dogList = [
   {
     name: 'Cockapoo',
+    types: ['toy', 'mini', 'maxi'],
     image: 'https://images.dog.ceo/breeds/cockapoo/bubbles2.jpg'
   },
   {
     name: 'Eskimo',
+    types: ['american', 'canadian'],
     image: 'https://images.dog.ceo/breeds/eskimo/n02109961_10021.jpg' 
   },
   {
-    name: 'pointer',
+    name: 'Pointer',
+    types: ['italian', 'german', 'brittany', 'english', 'irish'],
     image: 'https://images.dog.ceo/breeds/husky/n02110185_10844.jpg'
   }
 ];
+
+/*
+for (var i = 0; i < dogList.length; i++) {
+  document.write(dogList[i].name + '  ')
+}
+*/
+
+dogList.forEach(function(dog) {
+  console.log(dog.name + ' has these types: ' + dog.types)
+});
